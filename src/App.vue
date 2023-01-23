436a45dc946df5571db5382c73e51f3c6e89f17f
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-01-23 11:15:24 +0100

    feat Add Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import Header from './components/Header.vue'
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  <Header />
-
-  <RouterView />
+  <div class="flex flex-col justify-between min-h-screen">
+    <Header />
+    <div class="flex-1">
+      <RouterView />
+    </div>
+    <Footer />
+  </div>
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="bg-slate-800 text-slate-200 text-center p-4 text-sm">
+    <p>&copy;{{ new Date().getFullYear() }} Portal. All rights reserved.</p>
+  </footer>
+</template>
